54e15d49f46da57e51bc7bc0cbbc0591a594b6c0
bexterdesign <office@bexter.rs>
2021-01-23 19:02:59 +0100

    test

--- resources/js/lang/en.js
+++ resources/js/lang/en.js
@@ -208,7 +208,7 @@ export default {
   },
   roles: {
     description: {
-      admin: 'Super Administrator. Have access and full permission to all pages.',
+      admin: 'Super Administrator. Have access and full permission to All pages.',
       manager: 'Manager. Have access and permission to most of pages except permission page.',
       editor: 'Editor. Have access to most of pages, full permission with articles and related resources.',
       user: 'Normal user. Have access to some pages',
