08e8d84538f7fd8d9f75e64bc08d28110ee484c9
bexterdesign <office@bexter.rs>
2021-05-10 22:43:43 +0200

    dorada menija

---
+++ resources/js/router/modules/selfProfile.js
@@ -0,0 +1,22 @@
+/** When your routing table is too long, you can split it into small modules**/
+import Layout from '@/layout';
+
+const selfProfileRoutes = {
+  path: '/profile',
+  component: Layout,
+  redirect: '/profile/edit',
+  meta: {
+    // permissions: ['manage permissions'],
+    roles: ['admin'],
+  },
+  children: [
+    {
+      path: 'edit',
+      component: () => import('@/views/users/SelfProfile'),
+      name: 'selfProfile',
+      meta: { title: 'userProfile', icon: 'people' },
+    },
+  ],
+};
+
+export default selfProfileRoutes;
